7b165fdb9d4ba952e41436727e5db9f60cf55571
RahmanAzriel <jiellowkey777@gmail.com>
2024-06-14 14:58:32 +0700

    bismillah

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@500&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Portofolio</title>
+</head>
+
+<body>
+    <header>
+        <div id="uwaw">
+            <nav>
+                <img src="images/kindpng_10596.png" alt="" class="logos-navos">
+                <ul class="listos-navos">
+                    <li><a href="#uwaw">Home</a></li>
+                    <li><a href="#skills">Skills</a></li>
+                    <li><a href="#certi">Certificate</a></li>
+                    <li><a href="#my-project">Project</a></li>
+                    <li><a href="#contact-wrap">Contact</a></li>
+                </ul>
+            </nav>
+    </header>
+    <main>
+        <section>
+            <div id="satuan-kesatuan">
+                <div class="names-dilos">
+                    <p><i>Hello!</i></p>
+                    <h1><span class="name-ani">Im Azriel Danu</span>
+                    <br>Im A Web Designer
+                </h1>
+                <div class="baden">
+                    <button class="baten-xixi"><a href="#contact-wrap">Contact me</a></button>
+                </div>
+            </div>
+            <div class="fodos-dilos">
+                <img src="images/kindpng_6630197.png" alt="" class="werck1">
+            </div>
+            <div class="abot">
+                <h1>UI/UX Designer , Front End Developer , Minecraft Builder , Comission Leader </h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem alias perferendis tempore,
+                    cumque suscipit non velit. Vitae at temporibus magni, quam, dignissimos optio nostrum cum rerum
+                    officia sequi laborum expedita</p>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div id="skills">
+                <div class="itiels">
+                    <div class="plain-skil">
+                        <article>
+                            <p class="jekils">SKILLS</p>
+                            <h1>What Im Great At</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, deleniti tempore! Deserunt
+                                incidunt obcaecati fugit officia animi debitis. Doloribus consectetur quasi laudantium a
+                                tempora sequi assumenda omnis, impedit unde sed?</p>
+                            </article>
+                        </div>
+                        <div class="carti-box">
+                            <div class="carti">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="lol1"
+                        viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                        d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5Zm-9.736 7.35v3.999h-.791v-1.714H1.79v1.714H1V11.85h.791v1.626h1.682V11.85h.79Zm2.251.662v3.337h-.794v-3.337H4.588v-.662h3.064v.662H6.515Zm2.176 3.337v-2.66h.038l.952 2.159h.516l.946-2.16h.038v2.661h.715V11.85h-.8l-1.14 2.596H9.93L8.79 11.85h-.805v3.999h.706Zm4.71-.674h1.696v.674H12.61V11.85h.79v3.325Z" />
+                    </svg>
+                    <p>Hyper Text Markup Language</p>
+                </div>
+                <div class="carti">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="lol1"
+                    viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                    d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM3.397 14.841a1.13 1.13 0 0 0 .401.823c.13.108.289.192.478.252.19.061.411.091.665.091.338 0 .624-.053.859-.158.236-.105.416-.252.539-.44.125-.189.187-.408.187-.656 0-.224-.045-.41-.134-.56a1.001 1.001 0 0 0-.375-.357 2.027 2.027 0 0 0-.566-.21l-.621-.144a.97.97 0 0 1-.404-.176.37.37 0 0 1-.144-.299c0-.156.062-.284.185-.384.125-.101.296-.152.512-.152.143 0 .266.023.37.068a.624.624 0 0 1 .246.181.56.56 0 0 1 .12.258h.75a1.092 1.092 0 0 0-.2-.566 1.21 1.21 0 0 0-.5-.41 1.813 1.813 0 0 0-.78-.152c-.293 0-.551.05-.776.15-.225.099-.4.24-.527.421-.127.182-.19.395-.19.639 0 .201.04.376.122.524.082.149.2.27.352.367.152.095.332.167.539.213l.618.144c.207.049.361.113.463.193a.387.387 0 0 1 .152.326.505.505 0 0 1-.085.29.559.559 0 0 1-.255.193c-.111.047-.249.07-.413.07-.117 0-.223-.013-.32-.04a.838.838 0 0 1-.248-.115.578.578 0 0 1-.255-.384h-.765ZM.806 13.693c0-.248.034-.46.102-.633a.868.868 0 0 1 .302-.399.814.814 0 0 1 .475-.137c.15 0 .283.032.398.097a.7.7 0 0 1 .272.26.85.85 0 0 1 .12.381h.765v-.072a1.33 1.33 0 0 0-.466-.964 1.441 1.441 0 0 0-.489-.272 1.838 1.838 0 0 0-.606-.097c-.356 0-.66.074-.911.223-.25.148-.44.359-.572.632-.13.274-.196.6-.196.979v.498c0 .379.064.704.193.976.131.271.322.48.572.626.25.145.554.217.914.217.293 0 .554-.055.785-.164.23-.11.414-.26.55-.454a1.27 1.27 0 0 0 .226-.674v-.076h-.764a.799.799 0 0 1-.118.363.7.7 0 0 1-.272.25.874.874 0 0 1-.401.087.845.845 0 0 1-.478-.132.833.833 0 0 1-.299-.392 1.699 1.699 0 0 1-.102-.627v-.495ZM6.78 15.29a1.176 1.176 0 0 1-.111-.449h.764a.578.578 0 0 0 .255.384c.07.049.154.087.25.114.095.028.201.041.319.041.164 0 .301-.023.413-.07a.559.559 0 0 0 .255-.193.507.507 0 0 0 .085-.29.387.387 0 0 0-.153-.326c-.101-.08-.256-.144-.463-.193l-.618-.143a1.72 1.72 0 0 1-.539-.214 1 1 0 0 1-.351-.367 1.068 1.068 0 0 1-.123-.524c0-.244.063-.457.19-.639.127-.181.303-.322.527-.422.225-.1.484-.149.777-.149.304 0 .564.05.779.152.217.102.384.239.5.41.12.17.187.359.2.566h-.75a.56.56 0 0 0-.12-.258.624.624 0 0 0-.246-.181.923.923 0 0 0-.37-.068c-.216 0-.387.05-.512.152a.472.472 0 0 0-.184.384c0 .121.047.22.143.3a.97.97 0 0 0 .404.175l.621.143c.217.05.406.12.566.211.16.09.285.21.375.358.09.148.135.335.135.56 0 .247-.063.466-.188.656a1.216 1.216 0 0 1-.539.439c-.234.105-.52.158-.858.158-.254 0-.476-.03-.665-.09a1.404 1.404 0 0 1-.478-.252 1.13 1.13 0 0 1-.29-.375Z" />
+                </svg>
+            </svg>
+            <p>Cascading Style Sheets</p>
+        </div>
+        <div class="carti">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="lol1"
+            viewBox="0 0 16 16">
+            <path fill-rule="evenodd"
+            d="M14 4.5V14a2 2 0 0 1-2 2H8v-1h4a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM3.186 15.29a1.176 1.176 0 0 1-.111-.449h.765a.578.578 0 0 0 .255.384c.07.049.153.087.249.114.095.028.202.041.319.041.164 0 .302-.023.413-.07a.559.559 0 0 0 .255-.193.507.507 0 0 0 .085-.29.387.387 0 0 0-.153-.326c-.101-.08-.255-.144-.462-.193l-.619-.143a1.72 1.72 0 0 1-.539-.214 1.001 1.001 0 0 1-.351-.367 1.068 1.068 0 0 1-.123-.524c0-.244.063-.457.19-.639.127-.181.303-.322.528-.422.224-.1.483-.149.776-.149.305 0 .564.05.78.152.216.102.383.239.5.41.12.17.186.359.2.566h-.75a.56.56 0 0 0-.12-.258.624.624 0 0 0-.247-.181.923.923 0 0 0-.369-.068c-.217 0-.388.05-.513.152a.472.472 0 0 0-.184.384c0 .121.048.22.143.3a.97.97 0 0 0 .405.175l.62.143c.218.05.406.12.566.211.16.09.285.21.375.358.09.148.135.335.135.56 0 .247-.063.466-.188.656a1.216 1.216 0 0 1-.539.439c-.234.105-.52.158-.858.158-.254 0-.476-.03-.665-.09a1.404 1.404 0 0 1-.478-.252 1.13 1.13 0 0 1-.29-.375Zm-3.104-.033A1.32 1.32 0 0 1 0 14.791h.765a.576.576 0 0 0 .073.27.499.499 0 0 0 .454.246c.19 0 .33-.055.422-.164.092-.11.138-.265.138-.466v-2.745h.79v2.725c0 .44-.119.774-.357 1.005-.236.23-.564.345-.984.345a1.59 1.59 0 0 1-.569-.094 1.145 1.145 0 0 1-.407-.266 1.14 1.14 0 0 1-.243-.39Z" />
+        </svg>
+        <p>JavaScript</p>
+    </div>
+</div>
+<div class="under-carti">
+    <div class="ancart">
+        <h2>80%</h2>
+        <p>Skill I Have</p>
+    </div>
+    <div class="ancart">
+        <h2>70%</h2>
+        <p>Skill I Have</p>
+    </div>
+    <div class="ancart">
+        <h2>0%</h2>
+        <p>Skill I Have</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div id="certi">
+            <div class="cerrrti">
+                <div class="certi-ket">
+                    <h4>Certificate</h4>
+                    <h1>Certificate That I Have</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum error labore, magni ab qui, a quis
+                        beatae at cupiditate corrupti, praesentium vel alias voluptate ipsum nam consequatur. Id, est
+                        ea. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae illo sapiente quis doloremque
+                        quo ullam commodi atque quidem amet dignissimos officiis soluta laudantium architecto hic, in
+                        sed quas maxime labore!</p>
+                    </div>
+            </div>
+            <div class="keseluruhan-certi">
+                <div class="deretan-certi">
+                    <div class="certi-first">
+                        <div class="certi-card">
+                            <img src="images/Screenshot 2023-11-15 104144.png" alt="" class="for-certi-1">
+                            <div class="ket-at-cer">
+                                <h1>Dicoding Begginer Certificate</h1>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus recusandae rerum
+                                    dolores, atque laudantium dolore ut maxime beatae repellendus pariatur asperiores!
+                                    Nihil, rem reprehenderit? Perspiciatis incidunt pariatur provident repellendus ab!
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempore quasi tempora?
+                                    Unde distinctio temporibus tempora, nam sed totam quia laboriosam? Fugit, cupiditate
+                                    rem voluptatum debitis corporis optio quae sequi!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="certi-first">
+                            <div class="certi-card">
+                                <img src="images/Screenshot 2023-11-15 104144.png" alt="" class="for-certi-1">
+                            <div class="ket-at-cer">
+                                <h1>Dicoding Begginer Certificate</h1>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus recusandae rerum
+                                    dolores, atque laudantium dolore ut maxime beatae repellendus pariatur asperiores!
+                                    Nihil, rem reprehenderit? Perspiciatis incidunt pariatur provident repellendus ab!
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempore quasi tempora?
+                                    Unde distinctio temporibus tempora, nam sed totam quia laboriosam? Fugit, cupiditate
+                                    rem voluptatum debitis corporis optio quae sequi!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="certi-column">
+                        <div class="column-card">
+                            <img src="images/Screenshot 2023-11-15 104144.png" alt="" class="col-certi">
+                            <div class="ket-card-column">
+                                <h1>Certificate Coming Soon</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat similique atque
+                                    odit. Eius, nihil porro, praesentium deleniti quos nam dicta nesciunt ducimus
+                                    perferendis delectus ullam odio! Eligendi odit est alias.</p>
+                            </div>
+                        </div>
+                        <div class="column-card">
+                            <img src="images/Screenshot 2023-11-15 104144.png" alt="" class="col-certi">
+                            <div class="ket-card-column">
+                                <h1>Certificate Coming Soon</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat similique atque
+                                    odit. Eius, nihil porro, praesentium deleniti quos nam dicta nesciunt ducimus
+                                    perferendis delectus ullam odio! Eligendi odit est alias.</p>
+                            </div>
+                        </div>
+                        <div class="column-card">
+                            <img src="images/Screenshot 2023-11-15 104144.png" alt="" class="col-certi">
+                            <div class="ket-card-column">
+                                <h1>Certificate Coming Soon</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat similique atque
+                                    odit. Eius, nihil porro, praesentium deleniti quos nam dicta nesciunt ducimus
+                                    perferendis delectus ullam odio! Eligendi odit est alias.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div id="my-project">
+                <div class="proj">
+                <div class="proj-ket">
+                    <p>This</p>
+                    <h1>My Lastest Project</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem non deserunt quisquam officiis
+                        magnam, quaerat expedita, in, asperiores perspiciatis sapiente similique! Odio ad expedita
+                        magnam est nesciunt laboriosam sit corrupti!</p>
+                </div>
+            </div>
+            <div class="img-ket-pro">
+                <div class="isian-ket">
+                    <div class="ket-mypro">
+                        <h1>My Project</h1>
+                        <p>lastest project i've been done</p>
+                    </div>
+                    <div class="woik-ket">
+                        <img src="images/imageedit_2_5486405537.jpg" alt="" class="pro-foto">
+                    </div>
+                </div>
+                <div class="isian-ket">
+                    <div class="woik-ket">
+                        <img src="images/imageedit_2_5486405537.jpg" alt="" class="pro-foto">
+                    </div>
+                    <div class="ket-mypro">
+                        <h1>My Project</h1>
+                        <p>lastest project i've been done</p>
+                    </div>
+                </div>
+                <div class="isian-ket">
+                    <div class="ket-mypro">
+                        <h1>My Project</h1>
+                        <p>lastest project i've been done</p>
+                    </div>
+                    <div class="woik-ket">
+                        <img src="images/imageedit_2_5486405537.jpg" alt="" class="pro-foto">
+                    </div>
+                </div>
+                <div class="isian-ket">
+                    <div class="woik-ket">
+                        <img src="images/imageedit_2_5486405537.jpg" alt="" class="pro-foto">
+                    </div>
+                    <div class="ket-mypro">
+                        <h1>My Project</h1>
+                        <p>lastest project i've been done</p>
+                    </div>
+                </div>
+                <div class="isian-ket">
+                    <div class="ket-mypro">
+                        <h1>My Project</h1>
+                        <p>lastest project i've been done</p>
+                    </div>
+                    <div class="woik-ket">
+                        <img src="images/imageedit_2_5486405537.jpg" alt="" class="pro-foto">
+                    </div>
+                </div>
+                <div class="isian-ket">
+                    <div class="woik-ket">
+                        <img src="images/imageedit_2_5486405537.jpg" alt="" class="pro-foto">
+                    </div>
+                    <div class="ket-mypro">
+                        <h1>My Project</h1>
+                        <p>lastest project i've been done</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div id="contact-wrap">
+            <div class="con-ket">
+                <div class="con-ket-kes">
+                    <h1>Hello Contact Me Here</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque inventore eveniet qui culpa dicta, unde vel repellendus laborum quasi vitae cupiditate aliquid magnam? Rerum exercitationem sed iste consectetur enim accusantium! lorem</p>
+                </div>
+            </div>
+            <div class="con-items">
+                <div class="con-image">
+                    <img src="images/imageedit_1_4068265754.png" alt="" class="Con-face">
+                </div>
+                <div class="contact-list">
+                    <div class="con-logo">
+                        <h1>Contact Me Here</h1>
+                    </div>
+                    <div class="contact-list-items">
+                        <div class="contact-column1">
+                            <div class="isian-items">
+                                <h2>First Name</h2>
+                                <label for="fname"></label>
+                                <input type="text" id="fname" name="fname" placeholder="First Name">
+                            </div>
+                            <div class="isian-items">
+                                <h2>Email</h2>
+                                <label for="Emails"></label>
+                                <input type="text" id="Emails" name="Emails" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="contact-column2">
+                            <div class="isian-items">
+                                <h2>Last Name</h2>
+                                <label for="Lname"></label>
+                                <input type="text" id="Lname" name="fname" placeholder="Last Name">
+                            </div>
+                            <div class="isian-items">
+                                <h2>Phone Number</h2>
+                                <label for="Nambar"></label>
+                                <input type="text" id="Nambar" name="Emails" placeholder="Phone Number">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sambit">
+                        <Button>Submit Here!</Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    </main>
+    <footer>
+        <h1>COPYRIGHT | AZRIEL DANU 2023</h1>
+    </footer>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/iciwiwir', function () {
+    return view('master');
+});
